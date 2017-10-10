4fdb00ce704ba28c1e7eeda2ad512cabf4f7c621
David <davatata@gmail.com>
2017-10-09 21:08:41 -0700

    home text is separate rows

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,33 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  words = ['code', 'build', 'create', 'work'];
+  words = ['code', 'build', 'create', 'innovate'];
   innerCurly:string[] = ['c','o','d','e'];
   wordOrder = 0;
 
   constructor() { }
 
   ngOnInit() {
-    setInterval(() => {
-      this.wordOrder = this.wordOrder + 1 >= this.words.length ? 0 : this.wordOrder + 1; // 1
-      let word = this.words[this.wordOrder].split('');  // ['b','u','i','l','d']
-      let cleared = false;
-      let timer = setInterval(() => {
-        if(this.innerCurly.length === 0 && !cleared) {
-          cleared = true;
-        } else if(cleared) {
-          this.innerCurly.push(word.shift());
-          if(word.length === 0) {
-            cleared = false;
-            clearInterval(timer);
+    let wordChange = setInterval(() => {
+        this.wordOrder = this.wordOrder + 1 >= this.words.length ? 0 : this.wordOrder + 1; // 1
+        let word = this.words[this.wordOrder].split('');  // ['b','u','i','l','d']
+        let cleared = false;
+        let timer = setInterval(() => {
+          if(this.innerCurly.length === 0 && !cleared) {
+            cleared = true;
+          } else if(cleared) {
+            this.innerCurly.push(word.shift());
+            if(word.length === 0) {
+              cleared = false;
+              clearInterval(timer);
+            }
+          } else {
+            this.innerCurly.pop();
           }
-        } else {
-          this.innerCurly.pop();
-        }
-      }, 200);
+        }, 200);
     }, 4000);
   }
-
-
 
 }
