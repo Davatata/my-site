5c21120572cd37420ea25389ca2e3d46168a731b
David <Davatata@users.noreply.github.com>
2018-02-12 14:50:58 -0800

    changed some styles. Created ngFor loops to loop over an array of tools/experiences

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, style } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +6,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  
+  testColor = "cornflowerblue";
+  words: string[] = ['code', 'build', 'create', 'innovate'];
+  innerCurly: string[] = ['c','o','d','e'];
+  wordOrder: number = 0;
+  experienceItems: experienceItem[] = [
+    { 
+      icon: "fa-html5",
+      color: "orange", 
+      header: "HTML5", 
+      text: "I use the newest version of HTML to keep up with the always evolving web standard so I can bring simple and clean pages to viewers."},
+    { 
+      icon: "fa-css3",
+      color: "cornflowerblue", 
+      header: "CSS3", 
+      text: "I've created UI's using popular CSS frameworks such as Foundation and Bootstrap. I strive for a smooth and clean experience."},
+    { 
+      icon: "fa-code",
+      color: "lightcoral", 
+      header: "JavaScript", 
+      text: "I'm creating pages using the newest versions of JavaScript. Coding all future pages with the Angular 4+ framework is a priority."}
+  ];
 
-  words:string[] = ['code', 'build', 'create', 'innovate'];
-  innerCurly:string[] = ['c','o','d','e'];
-  wordOrder:number = 0;
+  toolItems: toolItem[] = [
+    { 
+      image: "../../assets/angular.png",
+      style: {"width":"120px", "height":"120px"},
+      header: "Angular", 
+      text: "Angular is one of the most popular front-end frameworks in the market at the moment. It's simple and effective in developing single page applications."},
+    { 
+      image: "../../assets/bootstrap.png",
+      style: {"width":"120px", "height":"120px"},
+      header: "Bootstrap", 
+      text: "For styling purposes, Bootstrap is my choice since it is one of the most used framework for building responsive mobile-first sites."},
+    { 
+      image: "../../assets/typescript.png",
+      style: {"width":"120px", "height":"120px", "padding": "15px"},
+      header: "TypeScript", 
+      text: "TypeScript is an improvement on the JavaScript language and is the language of choice for working in the Angular environment."}
+  ];
 
   constructor() { }
 
@@ -34,4 +70,18 @@ export class HomeComponent implements OnInit {
     }, 4000);
   }
 
+}
+
+interface experienceItem {
+  icon:string, 
+  color:string, 
+  header:string, 
+  text:string
+}
+
+interface toolItem {
+  image:string,
+  style:{}, 
+  header:string, 
+  text:string
 }
