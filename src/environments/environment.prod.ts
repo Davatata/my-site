48974ec9f2e1315e33b68535ed0fb10b42953887
David <Davatata@users.noreply.github.com>
2018-03-30 20:36:34 -0700

    this guy put api keys >.>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyAJLjZkn061l0fTZviMWlTTvIsQnu3O3Vc",
-    authDomain: "portfolio-site-14b67.firebaseapp.com",
-    databaseURL: "https://portfolio-site-14b67.firebaseio.com",
-    projectId: "portfolio-site-14b67",
-    storageBucket: "portfolio-site-14b67.appspot.com",
-    messagingSenderId: "269122028486"
-  }
+  production: true
 };
